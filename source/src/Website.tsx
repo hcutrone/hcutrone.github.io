67ca4bdb7de8356d8a6b4f881d0abf44688db53c
HCutrone <hcutrone@calpoly.edu>
2022-10-09 19:56:52 -0700

    Add header

--- source/src/Website.tsx
+++ source/src/Website.tsx
@@ -1,8 +1,16 @@
-import { Text, Flex, Box, VStack } from '@chakra-ui/react';
+import { Text, Flex, Box, HStack, VStack, Spacer, Link, Image } from '@chakra-ui/react';
 
 const BackgroundText = () => {
   return (
     <Text fontSize="150px" m="0" bgGradient='linear(to-tl, #832cdd, #ce24ad)' bgClip="text">Hey there!</Text>
+  )
+};
+
+const LogoLink = ({ url, icon }: { url: string, icon?: string }) => {
+  return (
+    <Link href={url}>
+      <Image src={icon} alt="LogoLink" />
+    </Link>
   )
 };
 
@@ -18,6 +26,26 @@ const PageSection = ({ children, ...props }: any) => {
     >
       {children}
     </Flex>
+  );
+};
+
+const Header = () => {
+  return (
+    <HStack
+      w="100vw"
+      h="100px"
+      position="fixed"
+      backgroundColor="blackAlpha.500"
+      mt={-4}
+      ml={-4}
+      p={8}
+      spacing={10}
+    >
+      <Spacer />
+      <LogoLink url="https://www.github.com/hcutrone" icon="./Github-Mark-Light-32px.png" />
+      <LogoLink url="https://www.linkedin.com/in/hcutrone" icon="./linkedin.png" />
+      <Spacer />
+    </HStack>
   );
 };
 
@@ -48,6 +76,7 @@ export const Website = () => {
       textAlign="center"
       alignItems="center"
     >
+      <Header />
       <LandingBlock />
     </Box>
   );

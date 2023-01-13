fc198558a187964141627c97710339da30e2c930
HCutrone <hcutrone@calpoly.edu>
2023-01-13 12:22:04 -0800

    Refactor: Separate LandingBlock into its own component
    While doing so, let's also create a Shared file to house shared
    components, starting with the PageSection

--- frontend/src/Website.tsx
+++ frontend/src/Website.tsx
@@ -1,52 +1,8 @@
-import { Text, Flex, FlexProps, Box, HStack, VStack, Spacer, Link, Image, SimpleGrid, Icon, IconProps, Divider, Code, List, ListItem, ListIcon } from '@chakra-ui/react';
+import { Text, Flex, Box, HStack, VStack, Spacer, Link, SimpleGrid, Icon, Divider, Code, List, ListItem, ListIcon } from '@chakra-ui/react';
 import React from 'react';
 import { GradCap, Experience, Arrow_DownRight } from './Icons'
 import { getPersonalProjects, getSchoolProjects } from './projects';
-import { Header } from './components';
-
-const BackgroundText = () => {
-  return (
-    <Text fontSize="150px" m="0" bgGradient='linear(to-tl, #832cdd, #ce24ad)' bgClip="text">Hey there!</Text>
-  )
-};
-
-type PageSectionProps = {
-  children: React.ReactNode | React.ReactNode[] | undefined;
-};
-
-const PageSection = ({ children, ...flexProps }: PageSectionProps & FlexProps) => {
-  return (
-    <Flex
-      direction="column"
-      h="calc(100vh - 32px)"
-      lineHeight="1.3"
-      bgColor="blackAlpha.800"
-      borderRadius={10}
-      align="center"
-      {...flexProps}
-    >
-      {children}
-    </Flex>
-  );
-};
-
-const LandingBlock =() => {
-  return (
-    <PageSection>
-      <VStack m="auto" spacing={0}>
-        <Image src="./Harrison_Cutrone_Prop.jpg" boxSize={300} borderRadius="full" />
-        <BackgroundText />
-        <Text fontWeight="bold">I'm Harrison Cutrone.</Text>
-        <Text px={8} fontSize="4xl">
-          I'm a Software Engineering Apprentice at iFixit and a CS student at Cal Poly.
-        </Text>
-      </VStack>
-      <Text fontSize="3xl" pb={2} mx="auto">
-        Scroll down to see more...
-      </Text>
-    </PageSection>
-  );
-};
+import { Header, LandingBlock } from './components';
 
 type GridBlockProps = {
   header: string;
---
+++ frontend/src/components/Shared.tsx
@@ -0,0 +1,21 @@
+import { Flex, FlexProps } from "@chakra-ui/react";
+
+type PageSectionProps = {
+   children: React.ReactNode | React.ReactNode[] | undefined;
+ };
+
+export const PageSection = ({ children, ...flexProps }: PageSectionProps & FlexProps) => {
+   return (
+     <Flex
+       direction="column"
+       h="calc(100vh - 32px)"
+       lineHeight="1.3"
+       bgColor="blackAlpha.800"
+       borderRadius={10}
+       align="center"
+       {...flexProps}
+     >
+       {children}
+     </Flex>
+   );
+ };
